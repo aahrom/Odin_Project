80d928388dcbfe6203c9a414cb5dffc713465386
aahrom <ahromarreya@gmail.com>
2023-08-01 18:54:47 -0500

    Completed FizzBuzz problem

---
+++ library/myscript.js
@@ -0,0 +1,11 @@
+//book objects are going to be stored in a simple array
+let myLibrary = [];
+
+function Book(title, author){
+    this.title = title;
+    this.author = author;
+}
+
+function addBookToLibrary(){
+    
+}
